ef8e82c7eeeb4aa155d6068127d9e1e3990364f8
Patrik Vogel <patrik.vogel@paessler.com>
2022-06-04 18:21:11 +0900

    feat: useHead to add header info

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,25 @@
 <script setup lang="ts">
+import { isDark } from "~/composables/dark";
+
+useHead({
+  title: "Patrik Bird",
+  meta: [
+    { name: "description", content: "My personal website" },
+    {
+      name: "theme-color",
+      content: computed(() => (isDark.value ? "#00aba9" : "#ffffff")),
+    },
+  ],
+  // link: [
+  //   {
+  //     rel: "icon",
+  //     type: "image/svg+xml",
+  //     href: computed(() =>
+  //       preferredDark.value ? "/favicon-dark.svg" : "/favicon.svg"
+  //     ),
+  //   },
+  // ],
+});
 
 definePageMeta({
   layout: "home",
@@ -6,5 +27,6 @@ definePageMeta({
 </script>
 
 <template>
-  <h1>It's a small and simple test to see if nuxt SSR actually works :)</h1>
+  <h1>This will be my new Website</h1>
+  <h3>...using Nuxt 3 and UnoCSS</h3>
 </template>
