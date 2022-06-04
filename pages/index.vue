92e197969b18ec6e2579f5574a512184f8939984
Patrik Vogel <patrik.vogel@paessler.com>
2022-06-04 15:25:37 +0900

    chore: remove dead code

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
+
 definePageMeta({
   layout: "home",
 });
 </script>
 
 <template>
-  <h1>It's a small and simple test to see if nuxt SSR actually works :).</h1>
+  <h1>It's a small and simple test to see if nuxt SSR actually works :)</h1>
 </template>
